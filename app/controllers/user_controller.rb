a4457bd5f7a3caace2d8df993d3726234da0d106
TimeEmit <TimeEmit@sdf.lonestar.org>
2013-03-19 17:59:24 -0400

    Generated users_controller.  Not including destroy action as it is an unspecified sfunctionality.

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,19 @@
+class UserController < ApplicationController
+  def show
+  end
+
+  def index
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 HengageTest::Application.routes.draw do
+  get "user/show"
+
+  get "user/index"
+
+  get "user/new"
+
+  get "user/edit"
+
+  get "user/create"
+
+  get "user/update"
+
   root :to => 'users#new'
   devise_for :users
 end

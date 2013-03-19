5c6a1e1e81b202c91696e8553ac7d5d90d394095
TimeEmit <TimeEmit@sdf.lonestar.org>
2013-03-19 18:12:41 -0400

    NOTE: I seriously considered Ryan Bates' railscast on polymorphic associations for stateful users. See http://railscasts.com/episodes/394-sti-and-polymorphic-associations Ultimately decided that it was more robust of a solution than I needed for the task at hand.

--- db/migrate/20130319182807_add_admin_to_users.rb
+++ db/migrate/20130319182807_add_admin_to_users.rb
@@ -1,4 +1,8 @@
 class AddAdminToUsers < ActiveRecord::Migration
+  # NOTE: I seriously considered Ryan Bates' railscast on polymorphic associations for stateful users.
+  # http://railscasts.com/episodes/394-sti-and-polymorphic-associations
+  # Ultimately decided that it was more robust of a solution than I needed for the task at hand.
+
   def change
     add_column :users, :admin, :boolean, default: false
   end

178817cc106b776375ca0468e6f3f86185623fa6
TimeEmit <TimeEmit@sdf.lonestar.org>
2013-03-19 12:27:51 -0400

    Rails generation of application with postgresql (PGsql used to mimic HEngage working environment).

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HengageTest::Application.config.secret_token = 'fc3af8b2055ae8cc8384b94ce466018465eb1c93bc29cf61e65f4797c0a6386254de6599045afb4f60f0a22720ce1fd87b923f9ab34294a60b13354a8042ec79'

ad3318aa45e6853cf5bbe4bd41c38d51a626313a
TimeEmit <TimeEmit@sdf.lonestar.org>
2013-03-19 17:42:43 -0400

    Added short syntax for RSpec.  Testing to make sure emails cannot just be simple strings.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,11 @@ require 'spec_helper'
 describe User do
   it { should validate_presence_of(:email) }
 
+  it 'should not simple strings as emails' do 
+    build(:user, :email => 'hello').valid?.should be_false
+  end
+
   it 'should have a default password of foobar' do
-    FactoryGirl.create(:user).password.should eql 'foobar'
+    create(:user).password.should eql 'foobar'
   end
 end

eb7d61c7f673550ddf02e054210145c20ec18034
TimeEmit <TimeEmit@sdf.lonestar.org>
2013-03-20 11:44:29 -0400

    Generated scaffold for projects.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 HengageTest::Application.routes.draw do
+  resources :projects
+
+
   root :to => 'users#index'
   devise_for :users
   resources :users, except: :destroy
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130319182807) do
+ActiveRecord::Schema.define(:version => 20130320153647) do
+
+  create_table "projects", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",              :default => "",    :null => false

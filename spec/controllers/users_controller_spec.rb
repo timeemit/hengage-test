4854d75a33c8a47afc24192f2a873ee717599b66
TimeEmit <TimeEmit@sdf.lonestar.org>
2013-03-19 20:51:58 -0400

    Tested UsersController for non-admin users.

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -76,4 +76,78 @@ describe UsersController do
       end
     end
   end  
+  
+  describe 'when the user is admin' do
+    let(:user) { create(:user) }
+    
+    before(:each) do 
+      user
+      sign_in user
+    end
+
+
+    describe "GET 'show'" do
+      it "returns http success" do
+        get 'show', id: user.id
+        response.should be_success
+        expect(response).to render_template("show")
+      end
+    end
+
+    describe "GET 'index'" do
+      it "returns http success" do
+        get 'index'
+        response.should be_success
+        expect(response).to render_template("index")
+      end
+    end
+
+    describe "GET 'new'" do
+      it "returns http success" do
+        get 'new'
+        response.should be_redirect
+      end
+    end
+
+    describe "GET 'edit'" do
+      it "returns http success" do
+        get 'edit', id: user.id
+        response.should be_redirect
+      end
+    end
+
+    describe "POST 'create'" do
+      let(:new_email) { 'new@hengage.com' }
+      
+      it "returns http redirect with valid params" do
+        post 'create', {user: {email: new_email}}
+        response.should be_redirect
+        User.count.should eql 1
+        User.last.email.should_not eql new_email
+      end
+
+      it "returns http succes with invalid params" do
+        post 'create', {user: {email: 'hello'}}
+        response.should be_redirect
+        User.last.email.should_not be_eql new_email
+      end
+    end
+
+    describe "PUT 'update'" do
+      let(:updated_email) { 'revised@hengage.com' }
+      
+      it "returns http redirect with valid params" do
+        put 'update', {id: user.id, user: {email: updated_email} } 
+        response.should be_redirect
+        user.reload.email.should_not eql updated_email
+      end
+
+      it "returns http success with invalid params" do 
+        put 'update', {id: user.id, user: {email: 'hello'} }
+        response.should be_redirect
+        User.last.email.should_not be_eql updated_email
+      end
+    end
+  end  
+  
 end
